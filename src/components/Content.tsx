99082350541fd5ebde8e0009afcf980c4a01399b
Manoel de Souza Rocha Neto <manoelrocha.neto.93@hotmail.com>
2022-02-12 22:14:57 -0300

    Adds componentization to challenge project

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
+import { GenreResponseProps, MovieProps } from '../@types/types';
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps,
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  const { movies, selectedGenre } = props;
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
+import { GenreResponseProps } from '../@types/types';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClickButton: Function,
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  const { genres, selectedGenreId, handleClickButton} = props;
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  );
 }
